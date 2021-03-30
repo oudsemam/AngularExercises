50b82aa0e0cd5c4d8f2b64acbd8d9a639532860f
Maggie Oudsema <maggieoudsema@gmail.com>
2021-03-29 22:10:10 -0400

    updated with apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PetInfoComponent } from './pet-info/pet-info.component';
+import { ExerciseTwoComponent } from './exercise-two/exercise-two.component';
+import { FormsModule } from '@angular/forms';
+import { UpDownCounterComponent } from './up-down-counter/up-down-counter.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetInfoComponent,
+    ExerciseTwoComponent,
+    UpDownCounterComponent,
+    NameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
